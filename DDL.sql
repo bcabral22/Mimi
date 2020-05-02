c2a5845dc46acf09b29ac5676079b1c20f918d77
bcabral22 <44854516+bcabral22@users.noreply.github.com>
2020-05-02 16:24:43 -0700

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -5,3 +5,11 @@ create table Employee (
       yearHired date not null,
       constraint employeePK Primary key (employeeID));
   
+create table fullTime(
+		employeeID varchar(20) not null,
+        healthcareInfo varchar(20),
+        weeklyRate int , 
+        constraint fullTimePK Primary key (employeeID),
+		constraint employeeFK foreign key (employeeID) references Employee (employeeID));
+	
+    
