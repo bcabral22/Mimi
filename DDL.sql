3ea54b1f7f34a8db9b5eee37154c26591f294baa
bcabral22 <44854516+bcabral22@users.noreply.github.com>
2020-05-02 15:35:01 -0700

    Create DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,7 @@
+create table Employee (
+      employeeID varchar(20) not null,
+      fname varchar(30) not null, 
+      lname varchar(30) not null,
+      yearHired date not null,
+      constraint employeePK Primary key (employeeID));
+      
