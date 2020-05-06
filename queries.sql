f10dd75898c39b85da262c1a0ecf73abe183df0f
Darroyo310 <54966538+Darroyo310@users.noreply.github.com>
2020-05-06 13:53:58 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,3 +2,12 @@
 select fname, lname, postalAddress, Type, email, phoneNumber
 from Customer_addresses_v
 ;
+
+-- 3 i think this should work
+SELECT employee.fname, employee.lname, recipe.recipeName FROM employee
+INNER JOIN recipe ON employee.employeeID = recipe.employeeID
+where employee.employeeID IN
+(SELECT employeeID FROM (
+SELECT employeeID, COUNT(*) AS "TOTAL" FROM recipe GROUP BY employeeID
+HAVING COUNT(*) > 3) souschef)
+ORDER BY employeeID ASC;
