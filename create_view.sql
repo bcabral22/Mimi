7c199bc1750a6c76c46792b700ceabc3aeb3d8de
Aparaxium <austingary123@gmail.com>
2020-05-06 12:32:35 -0700

    update views

--- create_view.sql
+++ create_view.sql
@@ -1,29 +1,29 @@
--- 1
+-- 1 Brown confirmed this wroks, so hopefully it does
 create view MenuItem_v as
-select MI.foodname, MI.spiciness, M.type,
+select MI.foodname, MI.spiciness, M.menuType,
 case
     when exists(select 'X' from menuItem MI2
                     where MI.foodname = MI2.foodname
-                    and MI.type = MI2.type) then MI.price
+                    and MI.menutype = MI2.menutype) then MI.price
     else "N/A"
 end
-from menuItem MI cross join Menu M;
+from menuItem MI cross join menu M;
 
--- 2
+-- 2 not correct yet
 create Customer_addresses_v as
-select *, "Private Customer" as Type
-from privateCustomer PC inner join customer C on
-PC.fname = C.fname, PC.lname = C.lname, PC.postalAddress = C.postalAddress
-union
 select *, "Corporate Customer" as Type
-from corporateCustomer CC inner join customer C on
-CC.fname = C.fname, CC.lname = C.lname, CC.postalAddress = C.postalAddress
+from corpCustomer CC inner join customer C on
+CC.fname = C.fname and CC.lname = C.lname and CC.postalAddress = C.postalAddress
+union
+select *, "N/A", "N/A", "N/A", "Private Customer" as Type
+from privateCustomer PC inner join customer C on
+PC.fname = C.fname and PC.lname = C.lname and PC.postalAddress = C.postalAddress
 ;
 
--- 3
+-- 3 I think this works
 create Sous_mentor_v as
 select E.fname as mentorFName, E.lname as mentorLName,
-E2.fname as sousFName, E.lname as sousLName
+E2.fname as sousFName, E.lname as sousLName,  M.foodName, M.startDate
 from Mentorship M inner join Employee E
 on M.mentorID = E.employeeID
 inner join  Employee E2
