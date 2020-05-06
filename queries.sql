508e10ca0d7488602a972cec9fb36c4712079f7d
Aparaxium <austingary123@gmail.com>
2020-05-06 13:47:50 -0700

    fix ddl, add date to party and party to bill

--- queries.sql
+++ queries.sql
@@ -2,3 +2,33 @@
 select fname, lname, postalAddress, Type, email, phoneNumber
 from Customer_addresses_v
 ;
+
+
+
+
+
+
+
+
+
+
+-- 11
+select OD.foodname, sum(MI.price)
+from orderdetails OD inner join menuitem MI on OD.foodname = MI.foodname
+group by OD.foodname
+;
+
+-- 12
+select max(c)
+from (
+select count(M.sousFName) as c
+from Sous_mentor_v M
+group by M.mentorFName) as a
+;
+
+
+-- 15
+select *
+from MenuItem_v M
+where M.price != "N/A"
+;
